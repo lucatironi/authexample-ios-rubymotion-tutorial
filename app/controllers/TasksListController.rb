4cf672f54cfea5772c6651ff95361fd1eda0cf7f
Luca Tironi <luca.tironi@gmail.com>
2013-05-18 15:37:17 +0200

    Added actual display of tasks in the TasksListController

--- app/controllers/TasksListController.rb
+++ app/controllers/TasksListController.rb
@@ -1,4 +1,5 @@
 class TasksListController < UIViewController
+  attr_accessor :tasks
 
   def self.controller
     @controller ||= TasksListController.alloc.initWithNibName(nil, bundle:nil)
@@ -6,6 +7,8 @@ class TasksListController < UIViewController
 
   def viewDidLoad
     super
+
+    self.tasks = []
 
     self.title = "Tasks"
     self.view.backgroundColor = UIColor.whiteColor
@@ -19,10 +22,48 @@ class TasksListController < UIViewController
     refreshButton = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemRefresh,
                                                                       target:self,
                                                                       action:'refresh')
-    self.navigationItem.rightBarButtonItem = refreshButton
+    self.navigationItem.rightBarButtonItems = [refreshButton]
+
+    @tasksTableView = UITableView.alloc.initWithFrame([[0, 0],
+                                                      [self.view.bounds.size.width, self.view.bounds.size.height]],
+                                                      style:UITableViewStylePlain)
+    @tasksTableView.dataSource = self
+    @tasksTableView.delegate = self
+    @tasksTableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
+
+    self.view.addSubview(@tasksTableView)
+
+    refresh if App::Persistence['authToken']
   end
 
+  # UITableView delegate methods
+  def tableView(tableView, numberOfRowsInSection:section)
+    self.tasks.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+
+    task = self.tasks[indexPath.row]
+
+    cell.textLabel.text = task.title
+
+    cell
+  end
+
+  # Controller methods
   def refresh
+    SVProgressHUD.showWithStatus("Loading", maskType:SVProgressHUDMaskTypeGradient)
+    Task.all do |jsonTasks|
+      self.tasks.clear
+      self.tasks = jsonTasks
+      @tasksTableView.reloadData
+      SVProgressHUD.dismiss
+    end
   end
 
   def logout
